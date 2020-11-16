55c2de31389c3c2d23791c96a79cc986cb537cf1
elizavetapavliv <pavliv.liza@yandex.ru>
2020-11-16 12:20:32 +0300

    Add scrinshots

---
+++ Assets/Flappy Bird Style/Scripts/Column.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Column : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<Bird>() != null)
+        {
+            GameController.instance.BirdScored();
+        }
+    }
+}
